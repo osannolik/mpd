83cdf2bf6d3b703443994e7e06d5b0685bce1465
Jonas <osannolik@godtycklig.se>
2020-10-25 13:45:04 +0100

    work

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@
 mod common;
 mod iir;
 mod mpd;
+mod resolve;
 
-use crate::common::{RangeDoppler, RangePulse, ScanProperties, Storable, Target, Units};
+use crate::common::{
+    CfarDetection, RangeDoppler, RangePulse, ScanProperties, Storable, Target, Units,
+};
 
 use crate::mpd::CfarConfig;
 use std::path::Path;
 
 fn main() {
-    let p = ScanProperties {
+    let mut p = ScanProperties {
         carrier_freq: 3.0e9,
         sample_freq: 1.0e6,
         duty_cycle: 0.1,
@@ -63,6 +66,21 @@ fn main() {
         .to_file(Path::new("range_doppler.json"))
         .expect("Could not write to file");
 
+    println!("Unambiguous_range {:?}", p.unambiguous_range());
+    println!("unambiguous_velocity {:?}", p.unambiguous_velocity());
+    println!(
+        "Aliased target 1 range velocity {:?} {:?} {:?}",
+        p.alias_range(targets[0].range),
+        p.alias_velocity(targets[0].velocity),
+        targets[0].level.value()
+    );
+    println!(
+        "Aliased target 2 range velocity {:?} {:?} {:?}",
+        p.alias_range(targets[1].range),
+        p.alias_velocity(targets[1].velocity),
+        targets[1].level.value()
+    );
+
     let cfg = CfarConfig {
         mainlobe_clutter_margin: 6,
         min_value: noise_level - 10.0.db(),
@@ -70,5 +88,62 @@ fn main() {
         n_rs_thr: 16,
         n_guard_bins: 1,
     };
-    let _x = range_doppler.cfar(&cfg, &p);
+    let cfar_detections = range_doppler.cfar(&cfg, &p);
+
+    println!("{:?}", cfar_detections);
+
+    let cfar_dets1 = vec![
+        CfarDetection {
+            range_bin: 93,
+            velocity: 16.1184,
+            level: 0.0.db(),
+        },
+        CfarDetection {
+            range_bin: 105,
+            velocity: 134.8854,
+            level: 0.0.db(),
+        },
+    ];
+
+    let cfar_dets2 = vec![
+        CfarDetection {
+            range_bin: 17,
+            velocity: 172.1081,
+            level: 0.0.db(),
+        },
+        CfarDetection {
+            range_bin: 32,
+            velocity: 450.9784,
+            level: 0.0.db(),
+        },
+    ];
+
+    let cfar_dets3 = vec![
+        CfarDetection {
+            range_bin: 44,
+            velocity: 216.2381,
+            level: 0.0.db(),
+        },
+        CfarDetection {
+            range_bin: 86,
+            velocity: 450.5799,
+            level: 0.0.db(),
+        },
+        CfarDetection {
+            range_bin: 99,
+            velocity: 100.5799,
+            level: 0.0.db(),
+        },
+    ];
+
+    let mut resolver = resolve::Resolver::new(200000.0, 8, 3, p.range_bin_length());
+
+    resolver
+        .add(&cfar_dets1, &p)
+        .add(&cfar_dets2, p.set_prf(9434.0))
+        .add(&cfar_dets3, p.set_prf(10309.0));
+
+    let res_dets = resolver.process(30.0, 1000.0);
+
+    println!("res_dets: {:?}", res_dets);
 }
